3830b0290ae5202048a50640a9855d6c85df3dbd
jaelomin <jaelomin@urbanbase.com>
2018-02-28 17:37:02 +0900

    add route model

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+var mongoose = require('mongoose');
+const book = require('./routes/book');
+
+
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://jaelomin:ex7804!!@ds155130.mlab.com:55130/jaelomin-db')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
